4d25cb62e5f118521f33905d28e3c1ec4927d23d
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2023-03-23 12:09:01 +0530

    formatted

--- models/inquiry.model.js
+++ models/inquiry.model.js
@@ -3,37 +3,37 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const InquirySchama = new Schema(
-    {
-        customerId: {
-            type: String,
-            required: false,
-        },
-        customerName: {
-            type: String,
-            required: true,
-        },
-        customerEmailAddress: {
-            type: String,
-            required: false,
-        },
-        customerMobileNumber: {
-            type: String,
-            required: true,
-        },
-        type: {
-            type: String,
-            required: true,
-        },
-        customerMessage: {
-            type: String,
-            required: true,
-        },
-        status: {
-            type: Boolean,
-            required: true,
-        },
+  {
+    customerId: {
+      type: String,
+      required: false,
     },
-    { timestamps: true, versionKey: false },
+    customerName: {
+      type: String,
+      required: true,
+    },
+    customerEmailAddress: {
+      type: String,
+      required: false,
+    },
+    customerMobileNumber: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    customerMessage: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
 );
 
 export const Bill = mongoose.model("Inquiry", InquirySchama);
