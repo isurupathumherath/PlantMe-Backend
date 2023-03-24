a5423a49f602189ee28686846e0e8f645e678ca8
miyurugunarathna <miyuru@spadesapp.io>
2023-03-24 16:21:57 +0530

    Feat: complete user API module

--- schema/user.schema.js
+++ schema/user.schema.js
@@ -16,3 +16,15 @@ export const LoginBodySchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
+
+export const UserIdSchema = Joi.object({
+  id: Joi.string().hex().length(24).required(),
+});
+
+export const UpdateUserSchema = Joi.object({
+  name: Joi.string().min(3).optional(),
+  email: Joi.string().email().optional(),
+  mobile: Joi.string().min(10).optional(),
+  password: Joi.string().optional(),
+  role: Joi.string().valid("SUPER_ADMIN", "ADMIN", "CONSUMER").optional(),
+});
