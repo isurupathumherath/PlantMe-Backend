94ac28413ebe66f00b4320a88da9a170fb6090b4
dilafar <it20784720@my.sliit.lk>
2023-03-24 14:03:35 +0530

    plant-management repository created

--- models/index.js
+++ models/index.js
@@ -1,2 +1,3 @@
 export * from "./user.model.js";
 export * from "./inquiry.model.js";
+export * from "./plant.model.js";
--- repository/index.js
+++ repository/index.js
@@ -1,2 +1,3 @@
 export * from "./user.repository.js";
 export * from "./inquiry.repository.js";
+export * from "./plant.repository.js";
